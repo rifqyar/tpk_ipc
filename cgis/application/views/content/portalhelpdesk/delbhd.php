37bc4061c66a50823bdb3abed6d3f2293d9b84f4
rifqyar <rifqyaditya55@gmail.com>
2026-07-20 19:06:42 +0700

    lupa import swal cdn

--- cgis/application/views/content/portalhelpdesk/delbhd.php
+++ cgis/application/views/content/portalhelpdesk/delbhd.php
@@ -66,6 +66,7 @@
 
     <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
     <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
     <script>
         $(document).ready(function() {
