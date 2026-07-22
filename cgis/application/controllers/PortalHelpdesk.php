9e80274b906048a1185ec08db38018a7876edebe
rifqyar <rifqyaditya55@gmail.com>
2026-07-22 10:03:45 +0700

    lupa hapus debugging

--- cgis/application/controllers/PortalHelpdesk.php
+++ cgis/application/controllers/PortalHelpdesk.php
@@ -128,7 +128,6 @@ class PortalHelpdesk extends CI_Controller
 	}
 	public function prosesbhd()
 	{
-		var_dump($this->input->post());die(); // Debugging line to check POST data
 		$tgl_dok = $this->input->post('tgl');
 		$no_dok = $this->input->post('no_dok');
 
